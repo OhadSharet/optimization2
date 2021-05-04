d30545ecf582e608a3ae90946ea4dac53617822b
OfirGilad <ofirgila@post.bgu.ac.il>
2021-05-04 18:53:45 +0300

    all questions solved uploaded all graphs photos

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def ex1b():
     A = _generate_matrix(n)
     b = np.random.rand(n, 1)
     x0 = np.array(np.zeros((n, 1), dtype=int))
+    test_Jacobi(A, b, x0, w=1.0)
     test_Jacobi(A, b, x0)
     test_Gauss_Seidel(A, b, x0)
     test_Steepest_Descent(A, b, x0)
@@ -22,8 +23,8 @@ def _generate_matrix(n=256):
     return A.transpose() * v * A + 0.1 * sparse.eye(n)
 
 
-def test_Jacobi(A, b, x0):
-    x_ans = Jacobi(A.toarray(), b, x0)
+def test_Jacobi(A, b, x0, w=0.35):
+    x_ans = Jacobi(A.toarray(), b, x0, w)
     print("Jacobi x: \n %s" % x_ans)
 
 
@@ -42,7 +43,7 @@ def test_Conjugate_Gradient(A, b, x0):
     print("Conjugate_Gradient x: \n %s" % x_ans)
 
 
-def print_graph(data_queue, total_iterations,title = "residual"):
+def print_graph(data_queue, total_iterations, graph_type, method, w=1.0):
     x = []
     y = []
     for i in range(total_iterations + 1):
@@ -50,11 +51,17 @@ def print_graph(data_queue, total_iterations,title = "residual"):
         y.append(data_queue.pop(0))
     fig, ax = plt.subplots()
     ax.semilogy(x, y)
-    ax.set_title(title)
+    if method == "Jacobi":
+        ax.set_title(method + " with W=" + str(w))
+    else:
+        ax.set_title(method)
+    ax.set_xlabel("iterations")
+    ax.set_ylabel(graph_type)
+    ax.legend([graph_type])
     plt.show()
 
 
-def _General_Iterative_Method(A, b, x0, M, N, max_iterations=99999, sigma=1e-2, w=1.0):
+def _General_Iterative_Method(A, b, x0, M, N, max_iterations=200, epsilon=1e-2, w=1.0, method="General Iterative"):
     """
     Ax = b , we need to find x
     :param A: matrix in R^(n*n) A=N+M
@@ -63,54 +70,55 @@ def _General_Iterative_Method(A, b, x0, M, N, max_iterations=99999, sigma=1e-2, 
     :param M: matrix in R^(n*n) A=N+M
     :param N: matrix in R^(n*n) A=N+M
     :param max_iterations: max_iterations
-    :param sigma: algorithm stops when the delta is smaller the sigma
+    :param epsilon: algorithm stops when the delta is smaller the epsilon
     :return: x^k as the solution
     """
 
     residual_queue = []
-    converges_queue = []
+    convergences_queue = []
     last_x = x0
     M_inverse = np.linalg.inv(M)
     curr_iter = 0
     while curr_iter < max_iterations:
-        curr_x = (1-w)*last_x + (w * M_inverse) @ (b - N @ last_x)
+        curr_x = (1 - w) * last_x + (w * M_inverse) @ (b - N @ last_x)
         c = np.linalg.norm(A @ curr_x - b, 2) / np.linalg.norm(b, 2)
-        converges_queue.append(np.linalg.norm(A @ curr_x - b, 2) / np.linalg.norm(A @ last_x - b, 2))
+        convergences_queue.append(np.linalg.norm(A @ curr_x - b, 2) / np.linalg.norm(A @ last_x - b, 2))
         residual_queue.append(np.linalg.norm(A @ curr_x - b, 2))
-        print(c)
-        if c < sigma:
-            print_graph(residual_queue, curr_iter)
-            print_graph(converges_queue,curr_iter,"converges")
+        if c < epsilon or curr_iter == max_iterations - 1:
+            print_graph(residual_queue, curr_iter, "residual", method, w)
+            print_graph(convergences_queue, curr_iter, "convergence rate", method, w)
+            print("Number of Iterations: " + str(curr_iter))
+            print("curr: " + str(curr_iter))
             return curr_x
         last_x = curr_x
         curr_iter += 1
     return "failed"
 
 
-def Jacobi(A, b, x0, max_iterations=99999, sigma=1e-3, w=0.35):
+def Jacobi(A, b, x0, w=0.35, max_iterations=200, epsilon=1e-3):
     D = np.diag(np.diag(A))
     U = np.triu(A) - D
     L = np.tril(A) - D
 
-    return _General_Iterative_Method(A, b, x0, D, L + U, max_iterations, sigma, w)
+    return _General_Iterative_Method(A, b, x0, D, L + U, max_iterations, epsilon, w, "Jacobi")
 
 
-def Gauss_Seidel(A, b, x0, max_iterations=99999, sigma=1e-2):
+def Gauss_Seidel(A, b, x0, max_iterations=200, epsilon=1e-2):
     D = np.diag(np.diag(A))
     U = np.triu(A) - D
     L = np.tril(A) - D
 
-    return _General_Iterative_Method(A, b, x0, D + L, U, max_iterations, sigma)
+    return _General_Iterative_Method(A, b, x0, D + L, U, max_iterations, epsilon, 1, "Gauss Seidel")
 
 
-def Steepest_Descent(A, b, x0, max_iterations=99999, sigma=1e-2):
+def Steepest_Descent(A, b, x0, max_iterations=200, epsilon=1e-2):
     """
     Ax = b , we need to find x
     :param A: matrix in R^(n*n) SPD
     :param b: the solution
     :param x0: first guess
     :param max_iterations: max_iterations
-    :param sigma: algorithm stops when the delta is smaller the sigma
+    :param epsilon: algorithm stops when the delta is smaller the epsilon
     :return: x^k as the solution
     """
 
@@ -118,19 +126,19 @@ def Steepest_Descent(A, b, x0, max_iterations=99999, sigma=1e-2):
     last_r = b - A @ x0
     curr_iter = 0
     residual_queue = []
-    converges_queue = []
+    convergences_queue = []
     while curr_iter < max_iterations:
         Ar = A @ last_r
         alpha = (last_r.transpose() @ last_r) / (last_r.transpose() @ Ar)
         curr_x = last_x + alpha * last_r
         curr_r = last_r - alpha * Ar
         c = np.linalg.norm(A @ curr_x - b, 2) / np.linalg.norm(b, 2)
-        converges_queue.append(np.linalg.norm(A @ curr_x - b, 2) / np.linalg.norm(A @ last_x - b, 2))
+        convergences_queue.append(np.linalg.norm(A @ curr_x - b, 2) / np.linalg.norm(A @ last_x - b, 2))
         residual_queue.append(np.linalg.norm(A @ curr_x - b, 2))
-        print(c)
-        if c < sigma:
-            print_graph(residual_queue, curr_iter)
-            print_graph(converges_queue,curr_iter,"converges")
+        if c < epsilon:
+            print_graph(residual_queue, curr_iter, "residual", "Steepest Descent")
+            print_graph(convergences_queue, curr_iter, "convergence rate", "Steepest Descent")
+            print("Number of Iterations: " + str(curr_iter))
             return curr_x
         last_x = curr_x
         last_r = curr_r
@@ -138,14 +146,14 @@ def Steepest_Descent(A, b, x0, max_iterations=99999, sigma=1e-2):
     return "failed"
 
 
-def Conjugate_Gradient(A, b, x0, max_iterations=100, sigma=1e-2):
+def Conjugate_Gradient(A, b, x0, max_iterations=200, epsilon=1e-2):
     """
     Ax = b , we need to find x
     :param A: matrix in R^(n*n) SPD
     :param b: the solution
     :param x0: first guess
     :param max_iterations: max_iterations
-    :param sigma: algorithm stops when the delta is smaller the sigma
+    :param epsilon: algorithm stops when the delta is smaller the epsilon
     :return: x^k as the solution
     """
 
@@ -154,18 +162,19 @@ def Conjugate_Gradient(A, b, x0, max_iterations=100, sigma=1e-2):
     last_p = last_r
     curr_iter = 0
     residual_queue = []
-    converges_queue = []
+    convergences_queue = []
     while curr_iter < max_iterations:
         Ap = A @ last_p
         alpha = (last_r.transpose() @ last_r) / (last_p.transpose() @ Ap)
         curr_x = last_x + alpha * last_p
         curr_r = last_r - alpha * Ap
         c = np.linalg.norm(A @ curr_x - b, 2) / np.linalg.norm(b, 2)
-        converges_queue.append(np.linalg.norm(A @ curr_x - b, 2) / np.linalg.norm(A @ last_x-b, 2))
+        convergences_queue.append(np.linalg.norm(A @ curr_x - b, 2) / np.linalg.norm(A @ last_x - b, 2))
         residual_queue.append(np.linalg.norm(A @ curr_x - b, 2))
-        if c < sigma:
-            print_graph(residual_queue, curr_iter)
-            print_graph(converges_queue, curr_iter, "converges")
+        if c < epsilon:
+            print_graph(residual_queue, curr_iter, "residual", "Conjugate Gradient")
+            print_graph(convergences_queue, curr_iter, "convergence rate", "Conjugate Gradient")
+            print("Number of Iterations: " + str(curr_iter))
             return curr_x
         beta = (curr_r.transpose() @ curr_r) / (last_r.transpose() @ last_r)
         last_p = curr_r + beta * last_p
@@ -205,17 +214,21 @@ def GMRES_1(A, b, x0, max_iterations=50):
     curr_x = last_x
     last_r = b - A @ x0
     curr_iter = 0
+    residual_queue = []
     while curr_iter < max_iterations:
         Ar = A @ last_r
         alpha = (last_r.transpose() @ Ar) / (Ar.transpose() @ Ar)
         curr_x = last_x + alpha * last_r
         curr_r = last_r - alpha * Ar
         c = np.linalg.norm(A @ curr_x - b, 2) / np.linalg.norm(b, 2)
-        print(c)
+        residual_queue.append(np.linalg.norm(A @ curr_x - b, 2))
+        if curr_iter == max_iterations - 1:
+            print_graph(residual_queue, curr_iter, "residual", "GMRES(1)")
         last_x = curr_x
         last_r = curr_r
         curr_iter += 1
-    print("iteration: " + str(curr_iter))
+    print("Number of Iterations: " + str(curr_iter))
+
     return curr_x
 
 
@@ -242,7 +255,7 @@ def ex4a():
                   [-1]])
     x0 = np.array(np.zeros((10, 1), dtype=int))
 
-    x = Jacobi(L, b, x0, max_iterations=99999, sigma=1e-5, w=1.0)
+    x = Jacobi(L, b, x0, max_iterations=200, epsilon=1e-5, w=0.96)
     print("x: \n %s" % x)
 
 
@@ -275,7 +288,7 @@ def ex4b():
                   [np.zeros((7, 3)), M2]])
     N = L - M
 
-    x = _General_Iterative_Method(L, b, x0, M, N, max_iterations=99999, sigma=1e-5, w=0.7)
+    x = _General_Iterative_Method(L, b, x0, M, N, max_iterations=200, epsilon=1e-5, w=0.7, method="Jacobi")
     print("x: \n %s" % x)
 
 
@@ -315,13 +328,13 @@ def ex4c():
                   [np.zeros((3, 7)), M3]])
     N = L_Swap - M
 
-    x = _General_Iterative_Method(L_Swap, b, x0, M, N, max_iterations=99999, sigma=1e-5, w=0.7)
+    x = _General_Iterative_Method(L_Swap, b, x0, M, N, max_iterations=200, epsilon=1e-5, w=0.7, method="Jacobi")
     print("x: \n %s" % x)
 
 
 if __name__ == "__main__":
-    ex1b()
-    #ex3c()
-    #ex4a()
-    #ex4b()
-    #ex4c()
+    # ex1b()
+    # ex3c()
+    # ex4a()
+    # ex4b()
+     ex4c()
